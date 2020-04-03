6c68ad7c12c2782d6e70a4fb575b72ba95039371
Warren Tyler Beagle <tylerbeagle34@gmail.com>
2020-04-03 19:33:57 -0400

    Adding Account Holder functionality

--- src/AccountHolder.java
+++ src/AccountHolder.java
@@ -1,4 +1,37 @@
 
-public class AccountHolder {
+public class AccountHolder extends User {
 
+	protected String username;
+	protected String password;
+	protected int age;
+	protected String paymentInfo;
+	
+	public AccountHolder(String name, String username, String password, int age, String paymentInfo) {
+		super(name);
+		this.username = username;
+		this.password = password;
+		this.age = age;
+		this.paymentInfo = paymentInfo;
+	}
+	
+	public void rateShow(int rating) {
+		
+	}
+	
+	public void rateTheater(int rating) {
+		
+	}
+	
+	public void rentTheater() {
+		
+	}
+	
+	public void saveShow(Show show) {
+		
+	}
+	
+	public void saveTheater(Theater theater) {
+		
+	}
+	
 }
