4357f5c8fb8e248ff68b668511e3fdb358e26a09
Warren Tyler Beagle <tylerbeagle34@gmail.com>
2020-04-03 19:29:23 -0400

    Adding Manager functionality

--- src/Manager.java
+++ src/Manager.java
@@ -1,4 +1,26 @@
 
-public class Manager {
+public class Manager extends User {
 
+	protected String employeeID;
+	protected int age;
+	protected String password;
+	
+	public Manager(String name, String employeeID, int age, String password) {
+		super(name);
+		this.employeeID = employeeID;
+		this.password = password;
+	}
+	
+	public void addShow(Show show) {
+		
+	}
+	
+	public void removeShow(Show show) {
+		
+	}
+	
+	public void editShow(Show show) {
+		
+	}
+	
 }
