4b05df74d5c5485b3f4481bf4aa754d9120aae1a
Warren Tyler Beagle <tylerbeagle34@gmail.com>
2020-04-03 19:25:35 -0400

    Adding User and Guest functionality

--- src/Guest.java
+++ src/Guest.java
@@ -1,4 +1,12 @@
 
-public class Guest {
+public class Guest extends User {
 
+	public Guest(String name) {
+		super(name);
+	}
+	
+	public void makeAccount(String name, int age, String username, String password, String paymentInfo) {
+		
+	}
+	
 }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,33 @@
 
 public class User {
+	
+	protected String name;
+	protected boolean isHandicap;
+	protected double discount;
+	protected ArrayList<Ticket> tickets;
+	
+	public User(String name) {
+		this.name = name;
+	}
+	
+	public void buyTickets(Show show, int numTickets) {
+		
+	}
+	
+	public void returnTickets(Show show, int numTickets) {
+		
+	}
+	
+	public void buyRefreshments(Refreshment refreshment) {
+		
+	}
+	
+	public void printTickets() {
+		
+	}
+	
+	public void loadTickets() {
+		
+	}
 
 }
