6ab93d2f047e5e986b403f94312f70056c9af026
Nicholas Mims <nicholaslmims@gmail.com>
2020-04-06 19:26:09 -0400

    added comments to the Refreshment and Ticket objects

--- src/Refreshment.java
+++ src/Refreshment.java
@@ -1,10 +1,33 @@
 
+/**
+ * Defines a Refreshment object and holds the object's information.
+ * @author Nicholas Mims
+ */
 public class Refreshment {
+	/**
+	 * The name of the refreshment.
+	 */
 	private String name;
+	/**
+	 * A description of the refreshment.
+	 */
 	private String description;
+	/**
+	 * The nutritional information of the refreshment.
+	 */
 	private String nutritionalInfo;
+	/**
+	 * The price of the refreshment.
+	 */
 	private double price;
 	
+	/**
+	 * Constructs a Refreshment object.
+	 * @param name				The name of the Refreshment.
+	 * @param description		A description of the Refreshment.
+	 * @param nutritionalInfo	The nutritional information of the refreshment.
+	 * @param price				The price of the refreshment.
+	 */
 	public Refreshment(String name, String description, String nutritionalInfo, double price) {
 		this.name = name;
 		this.description = description;
@@ -12,6 +35,10 @@ public class Refreshment {
 		this.price = price;
 	}
 	
+	/**
+	 * Returns the information of the Refreshment object in String format.
+	 * @return the Refreshment object in String format
+	 */
 	public String toString() {
 		return "A(n) " + name + " costs $" + price + ", is a(n) " + description +
 				" and its nutritional information follows: " + nutritionalInfo;
--- src/Ticket.java
+++ src/Ticket.java
@@ -1,13 +1,32 @@
 
+/**
+ * Defines a Ticket object and holds the object's information.
+ * @author Nicholas Mims
+ */
 public class Ticket {
+	/**
+	 * The Show the Ticket is for.
+	 */
 	private Show show;
+	/**
+	 * The price of the Ticket.
+	 */
 	private double price;
 	
+	/**
+	 * Constructs a Ticket object.
+	 * @param show	The show the Ticket is for.
+	 * @param price	The price of the Ticket.
+	 */
 	public Ticket(Show show, double price) {
 		this.show = show;
 		this.price = price;
 	}
 	
+	/**
+	 * Returns the information of the Ticket object in String format.
+	 * @return the Ticket object in String format
+	 */
 	public String toString() {
 		return "The ticket to " + show.getName() + " costs $" + price;
 	}
