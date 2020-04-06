2c48125e3cfd983ee1242c411c269a6c5a96f9b2
Nicholas Mims <nicholaslmims@gmail.com>
2020-04-06 19:10:57 -0400

    added Refreshment and ticket class

---
+++ src/Ticket.java
@@ -0,0 +1,14 @@
+
+public class Ticket {
+	private Show show;
+	private double price;
+	
+	public Ticket(Show show, double price) {
+		this.show = show;
+		this.price = price;
+	}
+	
+	public String toString() {
+		return("The ticket to " + show.getName() + " costs $" + price);
+	}
+}
