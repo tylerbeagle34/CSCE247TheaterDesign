5efeb82eb7041df92f050a3bfb62c1a6bab055b9
Nicholas Mims <nicholaslmims@gmail.com>
2020-04-07 23:32:30 -0400

    Edited Comments

--- src/Refreshment.java
+++ src/Refreshment.java
@@ -1,16 +1,28 @@
 
 /**
- * 
- * This class will construct a refreshment object
- *
+ * Defines a Refreshment object and holds the object's information.
+ * @author Garrison Davis, Nicholas Mims, Tyler Beagle
  */
 public class Refreshment {
+	/**
+	 * The name of the refreshment.
+	 */
 	protected String name;
+	/**
+	 * A description of the refreshment.
+	 */
 	protected String description;
+	/**
+	 * The nutritional information of the refreshment.
+	 */
 	protected String nutritionalInfo;
+	/**
+	 * The price of the refreshment.
+	 */
 	protected double price;
 	
 	/**
+	 * Constructs a Refreshment Object.
 	 * @param name			The name of the Refreshment.
 	 * @param description		A description of the Refreshment.
 	 * @param nutritionalInfo	The nutritional information of the refreshment.
