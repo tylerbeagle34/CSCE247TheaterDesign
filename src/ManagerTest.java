da37df21608ca769b9fd607a825bc13b38c190a4
Warren Tyler Beagle <tylerbeagle34@gmail.com>
2020-04-13 14:48:40 -0400

    Adding Manager Test

---
+++ src/ManagerTest.java
@@ -0,0 +1,29 @@
+/**
+ * JUnit Test for Manager Class
+ * @author Warren Beagle
+ */
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ManagerTest {
+
+	Theater theater = new Theater("Theater", 1);
+	Manager manager = new Manager("Manager", "Manager", "EmpID", 21, "Password", theater);
+	
+	@Test
+	public void testManagerConstructor() {
+		assertEquals("Manager", manager.name);
+		assertEquals("Manager", manager.type);
+		assertEquals("EmpID", manager.empID);
+		assertEquals(21, manager.age);
+		assertEquals("Password", manager.password);
+		assertEquals(theater, manager.theater);
+	}
+	
+	@Test
+	public void testManagerToString() {
+		assertEquals("\nUser Name: " + manager.name + "\tEmployee ID: " + manager.empID, manager.toString());
+	}
+
+}
