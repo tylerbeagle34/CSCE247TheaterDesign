241eea2c128c298422ff182dbb0c4253872d27c7
Garrison Davis <garrisondavis@garrisons-air.attlocal.net>
2020-04-14 11:46:54 -0400

    Movie Test to test Show toString

---
+++ src/MovieTest.java
@@ -0,0 +1,37 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MovieTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testToString() {
+		Theater theater = new Theater("Theater", 1);
+		Show show = new Movie(theater, "Name", "Description", "Genre", 5.00, 120.0, "PG", "1730", "Movie", 10, 10);
+		String toString = show.toString();
+		assertEquals("\nShow Name: Name\nDescription: Description\nGenre: Genre\nPrice: $5.0\nRating: PG" 
+				+ "\nTime: 120.0\nType: MOVIE\nTheater: Theater", toString);
+		
+	}
+
+}
